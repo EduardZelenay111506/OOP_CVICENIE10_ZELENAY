0036cc83e887bad05ced9514083485dc6d5bc9c0
Eduard Zelenay <xzelenay@stuba.sk>
2022-04-20 16:00:49 +0200

    return object

--- src/main/java/sk/stuba/fei/uim/oop/oop_cvicenie10_zelenay/HelloController.java
+++ src/main/java/sk/stuba/fei/uim/oop/oop_cvicenie10_zelenay/HelloController.java
@@ -1,4 +1,4 @@
-package sk.stuba.fei.uim.oop;
+package sk.stuba.fei.uim.oop.oop_cvicenie10_zelenay;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,8 +6,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    private int counter=0;
+
+
     @GetMapping("/hello")
     public String hello(){
         return "Hello world!";
+    }
+
+    @GetMapping("/helloObject")
+    public HelloResponse helloObject(){
+        return new HelloResponse(counter++,"hello world");
     }
 }
